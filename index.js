86edba33c6bc0c39df3369a1ed614dfffe2c66f7
comolokko13 <alinail13.ank@gmail.com>
2021-01-13 16:23:11 -0800

    delete is working now

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const methodOverride = require('method-override')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended : true}))
+app.use(methodOverride('_method'))
 
 //db connection
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/tasks', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
 db.once('open',function(){
   console.log("Connection Open!!");
-})
+})  
 const taskSchema = new mongoose.Schema({
   name: String,
   date: String
@@ -26,8 +29,28 @@ const firstTask = new Task({name:'DB',date:'12/01/2020'});
 console.log(firstTask.name + "  " + firstTask.date)
 
 
-app.get('/notion', (req, res) => {
-    res.render('notion')
+app.get('/notion', async (req, res) => {
+  const tasks = await Task.find({})
+  res.render('notion',{tasks})
+})
+app.get('/notion/:id',async (req,res) =>{
+  const task = await Task.findById(req.params.id)
+  res.render('notion' , {task})
+})
+
+app.post('/notion',async (req,res) =>{
+  console.log(req.body)
+  const task = new Task(req.body.task);
+  console.log(task)
+  await task.save();
+  res.redirect('notion')
+})
+
+app.delete('/notion/:id', async (req, res) => {
+  console.log(req.params)
+  const { id } = req.params;
+  await Task.findByIdAndDelete(id);
+  res.redirect('/notion');
 })
 app.get('/', (req, res) => {
     res.render('dashboard')
--- public/notion.js
+++ public/notion.js
@@ -1,3 +1,17 @@
+//db connection
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/tasks', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error:'));
+db.once('open',function(){
+  console.log("Connection Open!!");
+})  
+const taskSchema = new mongoose.Schema({
+  name: String,
+  date: String
+})
+const Task = mongoose.model('Task',taskSchema);
+
 
 
 function allowDrop(ev) {
@@ -20,14 +34,16 @@ function drop(ev) {
     });
  });
 
-function saveButton(){
-	var tasklabel = $( "#tasklabel").val();
-	var deadline = 	$( "#deadline").val();
-   $(".todo-column").append("<button id='task' type='button' class='btn w-100' draggable='true' ondragstart='drag(event)'><div class='container'><div class='row'><div class='col-1 p-0'><div style='border-radius:5px ; height: 75px; background-color: rgba(255, 118, 0); width: 5px;'></div></div><div class='col-11 m-auto'>"+tasklabel+"</div></div></div></button>");
-$("#closebutton").click();
-}
+// function saveButton(){
+// 	var tasklabel = $( "#tasklabel").val();
+// 	var deadline = 	$( "#deadline").val();
+//    $(".todo-column").append("<button id='task' type='button' class='btn w-100' draggable='true' ondragstart='drag(event)'><div class='container'><div class='row'><div class='col-1 p-0'><div style='border-radius:5px ; height: 75px; background-color: rgba(255, 118, 0); width: 5px;'></div></div><div class='col-11 m-auto'>"+tasklabel+"</div></div></div></button>");
+// $("#closebutton").click();
+// }
 
-function remove_me(element){
-  $('.delete_task_button').closest("button").remove();
+function remove_me(element,taskID){
+  alert(taskID);
+  console.log(await Task.findByIdAndDelete({_id : taskID}));
+  $('#task_'+ taskID).remove();
 } 
 
